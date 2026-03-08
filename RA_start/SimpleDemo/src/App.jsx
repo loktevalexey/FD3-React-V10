4047b76c847bf7a7493a8e634cdb9d14ac5c648d
loktev <loktev.alex.74@gmail.com>
2026-03-09 00:32:13 +0300

    RA_start

---
+++ RA_start/SimpleDemo/src/App.jsx
@@ -0,0 +1,13 @@
+import Warning from './components/Warning.jsx';
+
+function App() {
+  return (
+      <div>
+          <p>начало</p>
+          <Warning question="Не трогайте мокрыми руками оголённые провода!" />,
+          <p>конец</p>
+      </div>
+  )
+}
+
+export default App
---
+++ RA_start/SimpleDemo/src/components/Warning.jsx
@@ -0,0 +1,17 @@
+﻿import React from 'react';
+
+import './Warning.css';
+
+function Warning(props) {
+
+    return (
+      <div className='Warning'>
+        <span className='Warning_Text'>
+          {props.question}
+        </span>
+      </div>
+    );
+
+}
+
+export default Warning;
