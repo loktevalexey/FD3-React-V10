b65737c55dbb267eb99fc165357a2c35a885da95
loktev <loktev.alex.74@gmail.com>
2026-03-16 00:28:25 +0300

    RC254_callback

---
+++ RC_events_state/RC254_callback/src/components/VotesAnswer.jsx
@@ -0,0 +1,40 @@
+﻿import React from 'react';
+
+import './VotesAnswer.css';
+
+function VotesAnswer(props) {
+
+    function freeAnswerTextChanged(eo) {
+        console.log('VotesAnswer: текст свободного ответа изменён - '+eo.target.value);
+        props.cbFreeAnswerTextChanged(eo.target.value);
+    }
+
+    if ( props.workMode===1 ) {
+        return (
+            <div>
+                <label className='VotesBlockAnswer'>
+                    <input type='radio' value={props.code} name='voteanswer' />
+                    <span>{props.text}</span>
+                    {
+                        (props.freeanswer) &&
+                        <input type='text' name='votefreeanswer' className='FreeAnswer'
+                               defaultValue="???"
+                               onChange={freeAnswerTextChanged}
+                        />
+                    }
+                </label>
+            </div>
+        )
+    }
+    else {
+        return (
+            <div className='VotesBlockAnswer'>
+                <span className='Count'>{props.count}</span>
+                <span className='Text'>{props.text}</span>
+            </div>
+        );
+    }
+
+}
+
+export default VotesAnswer;
---
+++ RC_events_state/RC254_callback/src/components/VotesBlock.jsx
@@ -0,0 +1,32 @@
+﻿import React from 'react';
+
+import './VotesBlock.css';
+
+import VotesQuestion from './VotesQuestion.jsx';
+import VotesAnswer from './VotesAnswer.jsx';
+
+function VotesBlock(props) {
+
+    function freeAnswerTextChanged(fat) {
+        console.log('VotesBlock: текст свободного ответа изменён - '+fat);
+    }
+
+    const answersCode=props.answers.map( v =>
+        <VotesAnswer key={v.code}
+                     text={v.text} count={v.count} code={v.code}
+                     freeanswer={v.freeanswer}
+                     cbFreeAnswerTextChanged={freeAnswerTextChanged}
+                     workMode={props.workMode}
+        />
+    );
+
+    return (
+      <div className='VotesBlock'>
+        <VotesQuestion question={props.question}/>
+        <div className='Answers'>{answersCode}</div>
+      </div>
+    );
+
+}
+
+export default VotesBlock;
