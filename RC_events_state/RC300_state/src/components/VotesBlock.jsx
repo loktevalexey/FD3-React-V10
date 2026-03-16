55f93479417556fed6cfe0d76c485da45556fd0a
loktev <loktev.alex.74@gmail.com>
2026-03-16 23:05:13 +0300

    RC300_state

---
+++ RC_events_state/RC300_state/src/components/VotesBlock.jsx
@@ -0,0 +1,36 @@
+﻿import React, {useState} from 'react';
+
+import './VotesBlock.css';
+
+import VotesQuestion from './VotesQuestion.jsx';
+import VotesAnswer from './VotesAnswer.jsx';
+
+function VotesBlock(props) {
+
+    const [freeanswertext, setFreeanswertext] = useState( "" );
+
+    function freeAnswerTextChanged(fat) {
+        console.log('VotesBlock: текст свободного ответа изменён - '+fat);
+        setFreeanswertext(fat);
+    }
+
+    const answersCode=props.answers.map( v =>
+        <VotesAnswer key={v.code}
+                     text={v.text} count={v.count} code={v.code}
+                     freeanswer={v.freeanswer}
+                     cbFreeAnswerTextChanged={freeAnswerTextChanged}
+                     workMode={props.workMode}
+        />
+    );
+
+    return (
+      <div className='VotesBlock'>
+        <VotesQuestion question={props.question}/>
+        <div className='Answers'>{answersCode}</div>
+        <div>{freeanswertext}</div>
+      </div>
+    );
+
+}
+
+export default VotesBlock;
