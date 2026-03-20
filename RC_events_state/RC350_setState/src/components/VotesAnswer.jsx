f23ebb80db2fb48002a0969f4ba67fefd56ff186
loktev <loktev.alex.74@gmail.com>
2026-03-21 00:44:50 +0300

    RC350_setState

---
+++ RC_events_state/RC350_setState/src/components/VotesAnswer.jsx
@@ -0,0 +1,39 @@
+﻿import React from 'react';
+
+import './VotesAnswer.css';
+
+function VotesAnswer(props) {
+
+    function freeAnswerTextChanged(eo) {
+        props.cbFreeAnswerTextChanged(eo.target.value);
+    }
+
+    if ( props.workMode===1 ) {
+        return (
+            <div>
+                <label className='VotesBlockAnswer'>
+                    <input type='radio' value={props.code} name='voteanswer' />
+                    <span>{props.text}</span>
+                    {
+                        (props.freeanswer) &&
+                        <input type='text' name='votefreeanswer' className='FreeAnswer'
+                               defaultValue={props.deffreeanswertext}
+                               onChange={freeAnswerTextChanged}
+                        />
+                    }
+                </label>
+            </div>
+        )
+    }
+    else {
+        return (
+            <div className='VotesBlockAnswer'>
+                <span className='Count'>{props.count}</span>
+                <span className='Text'>{props.text}</span>
+            </div>
+        );
+    }
+
+}
+
+export default VotesAnswer;
---
+++ RC_events_state/RC350_setState/src/components/VotesBlock.jsx
@@ -0,0 +1,52 @@
+﻿import React, {useState} from 'react';
+
+import './VotesBlock.css';
+
+import VotesQuestion from './VotesQuestion.jsx';
+import VotesAnswer from './VotesAnswer.jsx';
+
+function VotesBlock(props) {
+
+    const [freeanswertext, setFreeanswertext] = useState(props.deffreeanswertext);
+    const [workMode, setWorkMode] = useState(props.startWorkMode);
+    const [cnt, setCnt] = useState(0);
+
+    function cntPlus3() {
+        setCnt(cnt+3);
+
+        // setCnt(cnt+1);
+        // setCnt(cnt+1);
+        // setCnt(cnt+1);
+
+        // setCnt(cnt => cnt+1);
+        // setCnt(cnt => cnt+1);
+        // setCnt(cnt => cnt+1);
+    }
+
+    function freeAnswerTextChanged(fat) {
+        console.log('VotesBlock: текст свободного ответа изменён - '+fat);
+        setFreeanswertext(fat);
+    }
+
+    const answersCode=props.answers.map( v =>
+        <VotesAnswer key={v.code}
+                     text={v.text} count={v.count} code={v.code}
+                     freeanswer={v.freeanswer}
+                     deffreeanswertext={props.deffreeanswertext}
+                     cbFreeAnswerTextChanged={freeAnswerTextChanged}
+                     workMode={workMode}
+        />
+    );
+
+    return (
+      <div className='VotesBlock'>
+        <VotesQuestion question={props.question}/>
+        <div className='Answers'>{answersCode}</div>
+        <div>{freeanswertext+" "+cnt}</div>
+        <input type="button" value="+=3" onClick={cntPlus3} />
+      </div>
+    );
+
+}
+
+export default VotesBlock;
