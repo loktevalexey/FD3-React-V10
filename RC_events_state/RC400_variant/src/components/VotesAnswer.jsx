2f250e4af5244b5e6195a8e355661b90980a25a3
loktev <loktev.alex.74@gmail.com>
2026-03-23 23:06:29 +0300

    RC400_variant

---
+++ RC_events_state/RC400_variant/src/components/VotesAnswer.jsx
@@ -0,0 +1,51 @@
+﻿import React from 'react';
+
+import './VotesAnswer.css';
+
+function VotesAnswer(props) {
+
+    function answerClicked(eo) {
+        props.cbSelected(props.code);
+
+        // как можно достучаться к атрибутам тега:
+        console.log(eo.target.getAttribute('value'));    // сработает с любым атрибутом
+        console.log(eo.target.getAttribute('data-kkk')); // сработает с любым атрибутом
+        console.log(eo.target.dataset.kkk);              // сработает только с data- атрибутом
+    }
+
+    function freeAnswerTextChanged(eo) {
+        props.cbFreeAnswerTextChanged(eo.target.value);
+    }
+
+    if ( props.workMode===1 ) {
+        return (
+            <div>
+                <label className='VotesBlockAnswer'>
+                    <input type='radio' value={props.code} name='voteanswer'
+                           onChange={answerClicked}
+                           data-kkk={props.code+100}
+                    />
+                    <span>{props.text}</span>
+                    {
+                        (props.freeanswer) &&
+                        <input type='text' name='votefreeanswer' className='FreeAnswer'
+                               defaultValue={props.deffreeanswertext}
+                               onChange={freeAnswerTextChanged}
+                        />
+                    }
+                </label>
+            </div>
+        )
+    }
+    else {
+        return (
+            <div className='VotesBlockAnswer'>
+                <span className='Count'>{props.count}</span>
+                <span className='Text'>{props.text}</span>
+            </div>
+        );
+    }
+
+}
+
+export default VotesAnswer;
---
+++ RC_events_state/RC400_variant/src/components/VotesBlock.jsx
@@ -0,0 +1,43 @@
+﻿import React, {useState} from 'react';
+
+import './VotesBlock.css';
+
+import VotesQuestion from './VotesQuestion.jsx';
+import VotesAnswer from './VotesAnswer.jsx';
+
+function VotesBlock(props) {
+
+    const [selectedAnswerCode,setSelectedAnswerCode]=useState(null);
+    const [freeanswertext, setFreeanswertext] = useState(props.deffreeanswertext);
+    const [workMode, setWorkMode] = useState(props.startWorkMode);
+
+    function answerSelected(code) {
+        console.log('выбран ответ с кодом '+code);
+        setSelectedAnswerCode(code);
+    }
+
+    function freeAnswerTextChanged(fat) {
+        console.log('VotesBlock: текст свободного ответа изменён - '+fat);
+        setFreeanswertext(fat);
+    }
+
+    const answersCode=props.answers.map( v =>
+        <VotesAnswer key={v.code}
+                     text={v.text} count={v.count} code={v.code}
+                     freeanswer={v.freeanswer}
+                     cbSelected={answerSelected}
+                     cbFreeAnswerTextChanged={freeAnswerTextChanged}
+                     workMode={workMode}
+        />
+    );
+
+    return (
+      <div className='VotesBlock'>
+        <VotesQuestion question={props.question}/>
+        <div className='Answers'>{answersCode}</div>
+      </div>
+    );
+
+}
+
+export default VotesBlock;
