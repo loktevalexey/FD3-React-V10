9baa88e70c46bb7bf96454809c56c768a821b433
loktev <loktev.alex.74@gmail.com>
2026-03-25 00:36:57 +0300

    RD601, RD602

---
+++ RD_twoWayBinding/RD601_selectedflag/src/components/VotesAnswer.jsx
@@ -0,0 +1,45 @@
+﻿import React from 'react';
+
+import './VotesAnswer.css';
+
+function VotesAnswer(props) {
+
+    function answerClicked(eo) {
+        props.cbSelected(props.code);
+    }
+
+    function freeAnswerTextChanged(eo) {
+        props.cbFreeAnswerTextChanged(eo.target.value);
+    }
+
+    if ( props.workMode===1 ) {
+        return (
+            <div>
+                <label className='VotesBlockAnswer'>
+                    <input type='radio' value={props.code} name='voteanswer'
+                           onChange={answerClicked} />
+                    <span>{props.text}</span>
+                    {
+                        (props.freeanswer) &&
+                        <input type='text' name='votefreeanswer' className='FreeAnswer'
+                               value={props.freeanswertext}
+                               onChange={freeAnswerTextChanged}
+                               disabled={!props.selectedFlag}
+                        />
+                    }
+                </label>
+            </div>
+        )
+    }
+    else {
+        return (
+            <div className='VotesBlockAnswer'>
+                <span className='Count'>{props.count}</span>
+                <span className='Text'>{props.text}</span>
+            </div>
+        );
+    }
+
+}
+
+export default VotesAnswer;
---
+++ RD_twoWayBinding/RD601_selectedflag/src/components/VotesBlock.jsx
@@ -0,0 +1,45 @@
+﻿import React, {useState} from 'react';
+
+import './VotesBlock.css';
+
+import VotesQuestion from './VotesQuestion.jsx';
+import VotesAnswer from './VotesAnswer.jsx';
+
+function VotesBlock(props) {
+
+    const [selectedAnswerCode,setSelectedAnswerCode]=useState(null);
+    const [freeanswertext, setFreeanswertext] = useState(props.deffreeanswertext);
+    const [workMode, setWorkMode] = useState(props.startWorkMode);
+
+    function answerSelected(code) {
+        console.log('выбран ответ с кодом '+code);
+        setSelectedAnswerCode(code);
+    }
+
+    function freeAnswerTextChanged(fat) {
+        console.log('VotesBlock: текст свободного ответа изменён - '+fat);
+        setFreeanswertext(fat);
+    }
+
+    const answersCode=props.answers.map( v =>
+        <VotesAnswer key={v.code}
+                     text={v.text} count={v.count} code={v.code}
+                     freeanswer={v.freeanswer}
+                     freeanswertext={freeanswertext}
+                     cbSelected={answerSelected}
+                     cbFreeAnswerTextChanged={freeAnswerTextChanged}
+                     selectedFlag={selectedAnswerCode===v.code}
+                     workMode={workMode}
+        />
+    );
+
+    return (
+      <div className='VotesBlock'>
+        <VotesQuestion question={props.question}/>
+        <div className='Answers'>{answersCode}</div>
+      </div>
+    );
+
+}
+
+export default VotesBlock;
