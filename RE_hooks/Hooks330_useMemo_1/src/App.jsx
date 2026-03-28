0154e0dead33daa422aab70df43cfde0426b1bb8
loktev <loktev.alex.74@gmail.com>
2026-03-29 00:22:26 +0300

    Hooks330_useMemo_*

---
+++ RE_hooks/Hooks330_useMemo_1/src/App.jsx
@@ -0,0 +1,16 @@
+import MobileCompany from './components/MobileCompany';
+
+let clientsArr=[ 
+  {id:101, fio:"Иванов И.И.", balance:200}, 
+  {id:105, fio:"Сидоров С.С.", balance:250}, 
+  {id:110, fio:"Петров П.П.", balance:180},
+  {id:120, fio:"Григорьев Г.Г.", balance:220},
+];
+
+function App() {
+  return (
+    <MobileCompany clients={clientsArr} />
+  );
+}
+
+export default App;
---
+++ RE_hooks/Hooks330_useMemo_3/src/components/MobileCompany.jsx
@@ -0,0 +1,54 @@
+﻿import React, { useState, useMemo } from 'react';
+
+import MobileClient from "./MobileClient";
+
+import "./MobileCompany.css";
+
+export default props => {
+
+  console.log("render MobileCompany");
+
+  const [companyName,setCompanyName] = useState("МТС");
+  const [clients,setClients]=useState(props.clients);
+
+  const clientsMemoizeed=useMemo(
+      () => clients.map( client =>
+          <MobileClient key={client.id} fio={client.fio} balance={client.balance} /> ),
+      [clients]
+  );
+
+  /*
+  а вот так хуки не работают:
+  for ( let i=0; i>clients.length; i++ ) {
+    const client=clients[i];
+    const clientMemoizeed=useMemo(
+      () => <MobileClient key={client.id} fio={client.fio} balance={client.balance} />,
+      [client.id,client.fio,client.balance]
+    );
+    ...
+  }
+  */
+
+  function sidorov() {
+    let newClients=clients.slice();
+    newClients.forEach( (client,index) => {
+      if ( client.id===105 ) {
+        let newClient={...client,balance:client.balance+1};
+        newClients[index]=newClient;
+      }
+    } );
+    setClients(newClients);
+  }
+
+  return (
+      <div className="MobileCompany">
+        <div>
+          <b>Компания: {companyName}</b><br/>
+          <input type="button" value="МТС" onClick={ ()=>setCompanyName("МТС") } />
+          <input type="button" value="A1" onClick={ ()=>setCompanyName("A1") } />
+        </div>
+        { clientsMemoizeed }
+        <input type="button" value="Сидоров++" onClick={sidorov} />
+      </div>
+  );
+};
