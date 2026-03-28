1308df603e473249ba03b75af0990a0a2f83d2e2
loktev <loktev.alex.74@gmail.com>
2026-03-29 00:39:40 +0300

    Hooks370_useContext

---
+++ RE_hooks/Hooks370_useContext/src/App.jsx
@@ -0,0 +1,12 @@
+import { First } from './components/First';
+
+function App() {
+  return (
+    <div>
+        <h1>Context example</h1>
+        <First />
+    </div>
+  );
+}
+
+export default App;
