3f29b9040602d6d6a7b6e2de5430ee3b9152894f
loktev <loktev.alex.74@gmail.com>
2026-03-29 00:31:31 +0300

    Hooks350_useCallback_*

---
+++ RE_hooks/Hooks350_useCallback_1/src/components/MobileCompany.jsx
@@ -0,0 +1,35 @@
+﻿import React, { useState } from 'react';
+
+import MobileClient from "./MobileClient";
+
+import "./MobileCompany.css";
+
+export default props => {
+
+  console.log("render MobileCompany");
+
+  const [clients,setClients]=useState(props.clients);
+
+  function changeBalance(clientId,delta) {
+    let newClients=clients.slice();
+    newClients.forEach( (client,index) => {
+      if ( client.id===clientId ) {
+        const newClient={...client,balance:client.balance+delta};
+        newClients[index]=newClient;
+      }
+    } );
+    setClients(newClients);
+  }
+
+  return (
+    <div className="MobileCompany">
+      {
+        clients.map( client => <MobileClient key={client.id} 
+          id={client.id} 
+          fio={client.fio} balance={client.balance} 
+          cbChangeBalance={changeBalance}
+          /> )
+      }
+    </div>
+  );
+};
