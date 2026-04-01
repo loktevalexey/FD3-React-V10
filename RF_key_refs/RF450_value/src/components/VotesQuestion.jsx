8aaf32d115fd34b9a25f269e85c46eae160567d9
loktev <loktev.alex.74@gmail.com>
2026-04-02 00:47:50 +0300

    RF450_value

---
+++ RF_key_refs/RF450_value/src/components/VotesQuestion.jsx
@@ -0,0 +1,26 @@
+﻿import React, {useEffect} from 'react';
+
+import './VotesQuestion.css';
+
+function VotesQuestion(props) {
+
+    // props->state при монтировании компонента
+    const [auxClassName, setAuxClassName] = React.useState(props.auxClassName);
+
+    useEffect(()=>{
+        console.log('VotesQuestion mount '+auxClassName);
+        return ()=>{
+            console.log('VotesQuestion unmount '+auxClassName);
+        };
+    },[]);
+
+    useEffect(()=>{
+        setAuxClassName(props.auxClassName);
+    },[props.auxClassName]);
+
+    console.log("VotesQuestion render props="+props.auxClassName+" state="+auxClassName);
+
+    return <input type="text" className={'VotesQuestion '+auxClassName} defaultValue={props.question} />;
+}
+
+export default VotesQuestion;
