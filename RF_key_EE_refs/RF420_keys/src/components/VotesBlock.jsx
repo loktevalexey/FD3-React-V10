038036f5c19543fc1a82cb565ae1cc54248533f9
loktev <loktev.alex.74@gmail.com>
2026-04-02 00:26:47 +0300

    RF420_keys

---
+++ RF_key_EE_refs/RF420_keys/src/components/VotesBlock.jsx
@@ -0,0 +1,80 @@
+﻿import React, {useState} from 'react';
+
+import './VotesBlock.css';
+
+import VotesQuestion from './VotesQuestion.jsx';
+import VotesAnswer from './VotesAnswer.jsx';
+
+function VotesBlock(props) {
+
+    const [selectedAnswerCode,setSelectedAnswerCode]=useState(null);
+    const [freeanswertext, setFreeanswertext] = useState(props.deffreeanswertext);
+    const [workMode, setWorkMode] = useState(props.startWorkMode);
+    const [answers,setAnswers]=useState(props.answers);
+    const [visibleQuestionNum, setVisibleQuestionNum]=useState(1);
+
+    function answerSelected(code) {
+        console.log('выбран ответ с кодом '+code);
+        setSelectedAnswerCode(code);
+    }
+
+    function vote() {
+        console.log('голосование завершено, выбран ответ с кодом '+selectedAnswerCode);
+        // глубокая копия через JSON - неэффективно! используем для простоты кода
+        const newAnswers=JSON.parse(JSON.stringify(answers));
+        newAnswers.forEach( answer => {
+            if ( answer.code===selectedAnswerCode )
+                answer.count++;
+        } );
+        setAnswers(newAnswers);
+        setWorkMode(2);
+    }
+
+    function freeAnswerTextChanged(fat) {
+        console.log('VotesBlock: текст свободного ответа изменён - '+fat);
+        setFreeanswertext(fat);
+    }
+
+    function showRedQuestion() {
+        setVisibleQuestionNum(1);
+    }
+
+    function showBlueQuestion() {
+        setVisibleQuestionNum(2);
+    }
+
+    const answersCode=answers.map( v =>
+        <VotesAnswer key={v.code}
+                     text={v.text} count={v.count} code={v.code}
+                     freeanswer={v.freeanswer}
+                     freeanswertext={freeanswertext}
+                     cbSelected={answerSelected}
+                     cbFreeAnswerTextChanged={freeAnswerTextChanged}
+                     selectedAnswerCode={selectedAnswerCode}
+                     workMode={workMode}
+        />
+    );
+
+    return (
+      <div className='VotesBlock'>
+        <input type="button" value="красный" onClick={showRedQuestion} />
+        <input type="button" value="синий" onClick={showBlueQuestion} />
+        {
+          (visibleQuestionNum===1) &&
+          <VotesQuestion key={1} question={props.question} auxClassName="VotesQuestionRed" />
+        }
+        {
+          (visibleQuestionNum===2) &&
+          <VotesQuestion key={2} question={props.question} auxClassName="VotesQuestionBlue" />
+        }
+        <div className='Answers'>{answersCode}</div>
+        {
+          ((workMode===1)&&selectedAnswerCode) &&
+          <input type='button' value='проголосовать' onClick={vote} />
+        }
+      </div>
+    );
+
+}
+
+export default VotesBlock;
---
+++ RF_key_EE_refs/RF420_keys/src/components/VotesQuestion.jsx
@@ -0,0 +1,19 @@
+﻿import React, {useEffect} from 'react';
+
+import './VotesQuestion.css';
+
+function VotesQuestion(props) {
+
+    useEffect(()=>{
+        console.log('VotesQuestion mount '+props.auxClassName);
+        return ()=>{
+            console.log('VotesQuestion unmount '+props.auxClassName);
+        };
+    },[]);
+
+    console.log('VotesQuestion render '+props.auxClassName);
+
+    return <div className={'VotesQuestion '+props.auxClassName}>{props.question}</div>;
+}
+
+export default VotesQuestion;
