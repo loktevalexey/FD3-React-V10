bd0d43a2a94931780b0488d14c0dec60dc6b7a4a
loktev <loktev.alex.74@gmail.com>
2026-04-02 00:31:17 +0300

    RF430_keys_state

---
+++ RF_key_refs/RF430_keys_state/src/components/VotesQuestion.jsx
@@ -0,0 +1,21 @@
+﻿import React, {useEffect} from 'react';
+
+import './VotesQuestion.css';
+
+function VotesQuestion(props) {
+
+    const [auxClassName, setAuxClassName] = React.useState(props.auxClassName);
+
+    useEffect(()=>{
+        console.log('VotesQuestion mount '+auxClassName);
+        return ()=>{
+            console.log('VotesQuestion unmount '+auxClassName);
+        };
+    },[]);
+
+    console.log('VotesQuestion render '+auxClassName);
+
+    return <div className={'VotesQuestion '+auxClassName}>{props.question}</div>;
+}
+
+export default VotesQuestion;
