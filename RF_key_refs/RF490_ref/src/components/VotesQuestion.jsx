9ee55f2f14b2ac46ac282970561bad9a685bf676
loktev <loktev.alex.74@gmail.com>
2026-04-03 22:16:18 +0300

    RF490_ref

---
+++ RF_key_refs/RF490_ref/src/components/VotesQuestion.jsx
@@ -0,0 +1,25 @@
+﻿import React, {useRef, useState} from 'react';
+
+import './VotesQuestion.css';
+
+function VotesQuestion(props) {
+
+    const [question,setQuestion]=useState(props.question);
+    const newTextRef = useRef();
+
+    function setNewText() {
+        if ( newTextRef.current ) {
+            setQuestion(newTextRef.current.value);
+        }
+    }
+
+    return (
+        <div>
+          <input type="text" defaultValue="новый текст вопроса" ref={newTextRef} />
+          <input type="button" value="задать" onClick={setNewText} />
+          <div className='VotesQuestion'>{question}</div>
+        </div>
+    );
+}
+
+export default VotesQuestion;
