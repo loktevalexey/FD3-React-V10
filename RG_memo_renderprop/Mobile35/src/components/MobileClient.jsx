02324d45bc481d47f0dfe2dad30092c76b8cd9a6
loktev <loktev.alex.74@gmail.com>
2026-04-06 23:50:53 +0300

    Mobile35

---
+++ RG_memo_renderprop/Mobile35/src/components/MobileClient.jsx
@@ -0,0 +1,14 @@
+﻿import './MobileClient.css';
+
+export default function MobileClient(props) {
+
+  console.log("MobileClient id="+props.info.id+" render");
+
+  return (
+    <div className='MobileClient'>
+      <span className='MobileClientBalance'>{props.info.balance}</span>
+      <span className='MobileClientFIO'>{props.info.fio}</span>
+    </div>
+  );
+
+}
