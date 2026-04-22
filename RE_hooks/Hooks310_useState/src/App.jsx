fec9fb6879cf0e903af71bdebec97073c8e29a00
loktev <loktev.alex.74@gmail.com>
2026-04-22 15:40:39 +0300

    StyledComponents75

--- RE_hooks/Hooks310_useState/src/App.jsx
+++
@@ -1,9 +0,0 @@
-import Anketa from './components/Anketa';
-
-function App() {
-  return (
-    <Anketa/>
-  );
-}
-
-export default App;
--- RG_memo_renderprop/Mobile35/src/components/VotesQuestion.jsx
+++
@@ -1,9 +0,0 @@
-﻿import React from 'react';
-
-import './VotesQuestion.css';
-
-function VotesQuestion(props) {
-    return <div className='VotesQuestion'>{props.question}</div>;
-}
-
-export default VotesQuestion;
