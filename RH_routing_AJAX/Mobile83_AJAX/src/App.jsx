e22b76930f99362b5bec8ddae8ba99924026ebda
loktev <loktev.alex.74@gmail.com>
2026-04-23 11:24:39 +0300

    Mobile83_AJAX

---
+++ RH_routing_AJAX/Mobile83_AJAX/src/App.jsx
@@ -0,0 +1,7 @@
+import MobileCompany from './components/MobileCompany';
+
+function App() {
+  return <MobileCompany />;
+}
+
+export default App;
---
+++ RH_routing_AJAX/Mobile83_AJAX/src/components/MobileCompany.jsx
@@ -0,0 +1,51 @@
+﻿import {useState,useEffect} from 'react';
+
+import MobileClient from './MobileClient';
+
+import './MobileCompany.css';
+
+export default function MobileCompany(props) {
+
+  const [isDataReady,setDataReady]=useState(false);
+  const [name,setName] = useState("???");
+  const [clients,setClients] = useState([]);
+
+  useEffect(()=>{
+    (async ()=>{
+        const response=await fetch("http://fe.it-academy.by/TestFetch.php", {
+          method: 'post',
+          headers: {
+            "Accept": "application/json",
+          },
+        });
+        // в response - http-ответ
+        if ( !response.ok ) {
+          alert("fetch error " + response.status);
+          return;
+        }
+        const data=await response.json();
+        // в data - пришедшие в ответе данные
+        console.log(data);
+        setName(data.companyName);
+        setClients(data.clientsArr);
+        setDataReady(true);
+    })();
+  },[]);
+
+  if ( !isDataReady )
+    return <div>загрузка данных...</div>;
+
+  const clientsCode=clients.map( client =>
+    <MobileClient key={client.id} info={client}  />
+  );
+
+  return (
+    <div className='MobileCompany'>
+      <div className='MobileCompanyName'>Компания &laquo;{name}&raquo;</div>
+      <div className='MobileCompanyClients'>
+        {clientsCode}
+      </div>
+    </div>
+  );
+
+}
