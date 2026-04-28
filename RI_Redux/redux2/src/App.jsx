e3f61a994483f34588af60240d706839faf9881e
loktev <loktev.alex.74@gmail.com>
2026-04-28 19:46:50 +0300

    redux2

---
+++ RI_Redux/redux2/src/App.jsx
@@ -0,0 +1,25 @@
+import combinedReducer from './redux/reducers.js';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+import { Buttons } from './components/Buttons';
+import { ShowInfo } from './components/ShowInfo';
+import { ProfileChange } from './components/ProfileChange';
+
+const store=createStore(combinedReducer);
+
+export const App = () => (
+  <Provider store={store}>
+    <div>
+      <h1>Redux example #2</h1>
+      <Buttons />
+      <br/><hr/><br/>
+      <ProfileChange/>
+      <br/><br/><hr/><br/>
+      <ShowInfo/>
+      <br/><br/>
+    </div>
+  </Provider>
+);
+
+export default App;
