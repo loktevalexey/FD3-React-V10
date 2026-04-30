55a1ea682a84263dff8540b9b2589ad35fb9b783
loktev <loktev.alex.74@gmail.com>
2026-04-30 15:48:17 +0300

    TS353_VotesBlock

---
+++ TC_TS3/TS353_VotesBlock/src/App.tsx
@@ -0,0 +1,16 @@
+import VotesBlock from './components/VotesBlock';
+
+import answersArr from './answers.json';
+
+const questionText='Как вы относитесь к программированию?';
+
+function App() {
+  return (
+    <VotesBlock 
+      question={questionText}
+      answers={answersArr}
+    />
+  );
+}
+
+export default App;
