9ba5755e4339d5fddbda5dde9eb6568c682916d8
loktev <loktev.alex.74@gmail.com>
2026-04-30 15:55:11 +0300

    TS415_VotesBlock

---
+++ TD_TS4/TS415_VotesBlock/src/App.tsx
@@ -0,0 +1,19 @@
+import VotesBlock from './components/VotesBlock';
+
+import answersArr from './answers.json';
+
+const questionText='Как вы относитесь к программированию?';
+const defaultFreeAnswerText="???";
+
+function App() {
+  return (
+    <VotesBlock 
+      question={questionText}
+      startAnswers={answersArr}
+      deffreeanswertext={defaultFreeAnswerText}
+      startWorkMode="VOTE"
+      />
+  );
+}
+
+export default App;
