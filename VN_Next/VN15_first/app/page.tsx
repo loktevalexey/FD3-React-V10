b8d9968fa1f7b96bf3bc67ccaff93611a9ea48ea
loktev <loktev.alex.74@gmail.com>
2026-05-10 22:38:04 +0300

    VN15_first

--- VN_Next/VN15_first/app/page.tsx
+++ VN_Next/VN15_first/app/page.tsx
@@ -1,8 +1,9 @@
 export default function Home() {
   return (
-    <div>
+    <div className="m-4">
       <p>Это корневая (и единственная) страница первого примера</p>
-      <p className="text-blue-500 border border-red-500 p-4">Она использует TailwindCSS</p>
+      <p className="text-blue-500 border border-red-500 p-4 my-4">Она использует TailwindCSS</p>
+      <img src="/logo3.png"/>
     </div>
   );
 }
